b00c74688952bde0ae2c7eabc0ebdf17048ddc50
Eric H <eheilig@perscholas.org>
2022-01-07 09:42:51 -0700

    database sql

---
+++ src/main/resources/db/migration/V0.0.01_Schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `user_roles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `user_role` varchar(45) COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ur_unique` (`user_role`,`user_id`) /*!80000 INVISIBLE */,
+  KEY `FK_user_id_idx` (`user_id`),
+  CONSTRAINT `FK_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) COLLATE utf8mb4_bin NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `password` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `first_name` varchar(45) COLLATE utf8mb4_bin DEFAULT NULL,
+  `last_name` varchar(45) COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
