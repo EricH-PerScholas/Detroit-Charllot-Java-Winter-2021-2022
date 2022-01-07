88cd07939ef036c614512ca0c3b04194508a71bf
Eric H <eheilig@perscholas.org>
2022-01-07 09:37:46 -0700

    formatting

--- src/main/java/perscholas/entity/User.java
+++ src/main/java/perscholas/entity/User.java
@@ -14,26 +14,27 @@ import javax.persistence.OneToMany;
 public class User {
 
 	@Id
-	// this annotation is what tells hibernate that this variable is an auto incremented primary key
+	// this annotation is what tells hibernate that this variable is an auto
+	// incremented primary key
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Integer id;
-	
+
 	@Column(name = "username")
 	private String username;
-	
+
 	@Column(name = "password")
 	private String password;
-	
+
 	@Column(name = "email")
 	private String email;
-	
+
 	@Column(name = "first_name")
 	private String firstName;
-	
+
 	@Column(name = "last_name")
 	private String lastName;
-	
+
 	@OneToMany(mappedBy = "user")
 	private List<UserRole> userRoles = new ArrayList<UserRole>();
 
@@ -84,7 +85,5 @@ public class User {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
-	
-	
+
 }
