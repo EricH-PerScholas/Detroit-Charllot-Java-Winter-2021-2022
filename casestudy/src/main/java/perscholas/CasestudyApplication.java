8b5c5971d826a9c895e05f734372c0f930545437
Eric H <eheilig@perscholas.org>
2022-01-24 12:48:12 -0700

    setting up spring boot

--- casestudy/src/main/java/perscholas/CasestudyApplication.java
+++ casestudy/src/main/java/perscholas/CasestudyApplication.java
@@ -2,9 +2,16 @@ package perscholas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CasestudyApplication {
+public class CasestudyApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CasestudyApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CasestudyApplication.class, args);
---
+++ casestudy/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+
+<body>
+
+<h1> this is our first JSP page</h1>
+
+</body>
+
+</html>
