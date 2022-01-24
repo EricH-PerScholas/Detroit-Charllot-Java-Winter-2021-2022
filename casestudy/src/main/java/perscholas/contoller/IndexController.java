45bb642b3dbc33d9f5a244857f85c9cb49885326
Eric H <eheilig@perscholas.org>
2022-01-24 12:17:25 -0700

    spring boot setup

---
+++ casestudy/src/main/java/perscholas/contoller/IndexController.java
@@ -0,0 +1,19 @@
+package perscholas.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+	public ModelAndView index() throws Exception {
+		ModelAndView response = new ModelAndView();
+		response.setViewName("index");
+
+		return response;
+	}
+	
+}
